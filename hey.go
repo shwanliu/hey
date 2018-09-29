5acf6cce8bc4ba130f36c23d8056e8d6357591c5
liuxiaoying <614477862@qq.com>
2018-09-29 13:16:06 +0800

    dsa

--- hey.go
+++ hey.go
@@ -235,6 +235,8 @@ func main() {
 
 		// Don't forget to close the multipart writer.
 		// If you don't close it, your request will be missing the terminating boundary.
+		bodyAll, err = json.Marshal(b)
+		// _, err = w.Write(bodyAll)
 		w.Close()
         _.err = bodyAll.write(b.bytes())
 		// Now that you have a form, you can submit it to your handler.
