b395c548600e4a610a23b03349137fb62df6c0be
liuxiaoying <614477862@qq.com>
2018-09-30 10:31:13 +0800

    fixed bug

--- hey.go
+++ hey.go
@@ -240,9 +240,8 @@ func main() {
 
 	// 分解掉两个文件名，各自分配一个变量(image，imageother)
 	var image,imageother string
-	var MultiFilename []byte
 	if *form_data_filename != "" {
-		MultiFilename = strings.Split(*form_data_filename,",")
+		MultiFilename := strings.Split(*form_data_filename,",")
 		fmt.Printf("%q\n", MultiFilename)
 		image = MultiFilename[0]
 		imageother = MultiFilename[1]
